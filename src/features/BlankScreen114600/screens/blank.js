8e932d707e50edd2683fa2105466200d99b4c6bb
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2020-11-17 19:04:29 -0800

    Update blank.js

--- src/features/BlankScreen114600/screens/blank.js
+++ src/features/BlankScreen114600/screens/blank.js
@@ -27,7 +27,7 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View style={styles.container}>
-      <Text>This is your new component 2</Text>
+      <Text>This is your new component Blank 2</Text>
     </View>
   );
 }
