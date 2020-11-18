760c113f55be647e9c3a7a15347b558889bab705
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2020-11-17 19:49:15 -0800

    Update index.js

--- src/features/SplashScreen/index.js
+++ src/features/SplashScreen/index.js
@@ -1,48 +1,84 @@
-import React, { Component } from "react";
-import { StyleSheet, Image, Text, ScrollView, TouchableOpacity } from "react-native";
+import React, {Component} from "react";
+import {StyleSheet, Image, ScrollView, TouchableOpacity, View} from "react-native";
+import {Text, Button} from "react-native-ui-kitten";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import LogoIcon from "../../assets/images/backgroundLoginV1.png";
-import { installed_blueprints } from "../../config/installed_blueprints";
-import { styles } from './styles'
+import {installed_blueprints} from "../../config/installed_blueprints";
+import {store} from "../../redux/store";
+import {styles} from './styles'
+import logo from "../../assets/icons/logo.png"
+import {connect} from 'react-redux';
 
-export default class App extends Component {
+class App extends Component {
 
-  static navigationOptions = {
-    title: 'Installed blueprints',
-    
-  };
+    static navigationOptions = {
+        title: 'Installed blueprints'
+    };
 
-  renderItems() {
-    const {
-      navigation: {navigate},
-    } = this.props;
+    componentDidMount() {
+        store.dispatch({type: 'TEST/ALO'});
+        const {navigation: {
+                navigate
+            }} = this.props;
+        if(this.props.accessToken){
+          navigate("BlankScreen15599")
+  //        navigate("MainApp")
+          //navigate("EmailAuth");
+        }
 
-    return installed_blueprints.map(item => {
-      if (item.hasOwnProperty('access_route')) {
+            
+        setTimeout(() => {
+          if(!this.props.accessToken){
+            navigate("EmailAuth")
+          }
+        }, 3330)
+    }
+
+    renderItems() {
+        const {navigation: {
+                navigate
+            }} = this.props;
+
+        return installed_blueprints.map(item => {
+            if (item.hasOwnProperty('access_route')) {
+                return (
+                    <TouchableOpacity
+                        onPress={_ => navigate(item.access_route)}
+                        style={styles.item}
+                        key={`${item.name}--blueprint-button`}>
+                        <Icon
+                            style={styles.itemLogo}
+                            name={item.icon_name
+                            ? item.icon_name
+                            : 'pencil-square-o'}
+                            size={40}
+                            color="#F88087"/>
+                        <Text style={styles.itemFont}>{item.human_name}</Text>
+                    </TouchableOpacity>
+                );
+            }
+        });
+    }
+
+    render() {
         return (
-          <TouchableOpacity
-            onPress={_ => navigate(item.access_route)}
-            style={styles.item}
-            key={`${item.name}--blueprint-button`}>
-            <Icon
-              style={styles.itemLogo}
-              name={item.icon_name ? item.icon_name : 'pencil-square-o'}
-              size={40}
-              color="#F88087"
-            />
-            <Text style={styles.itemFont}>{item.human_name}</Text>
-          </TouchableOpacity>
+            <View style={styles.itemsContainer}>
+                <Image
+                    style={{
+                    width: 365,
+                    height: 161,
+                }}
+                    resizeMode="contain"
+                    source={logo}/>
+                <Text style={styles.heading}>{"IwantSMART.com Rebate\nManagement Program"}</Text>
+            </View>
         );
-      }
-    });
-  }
-
-  render() {
-    return (
-      <ScrollView contentContainerStyle={styles.itemsContainer}>
-        {this.renderItems()}
-      </ScrollView>
-    );
-  }
+    }
 }
+
+const mapStateToProps = state => ({accessToken: state.EmailAuth.accessToken});
+
+const mapDispatchToProps = dispatch => ({actions: {}});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
