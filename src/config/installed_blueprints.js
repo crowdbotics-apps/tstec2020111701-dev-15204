06d38de3a1137ad0ae4d2ccdff00073c4dd8ef27
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2020-11-18 14:07:29 -0800

    Update installed_blueprints.js

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,7 +1,7 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
-{ name: 'BlankScreen114600', human_name: 'Blank Screen 1', access_route: 'BlankScreen114600'},
-{ name: 'BlankScreen014599', human_name: 'Blank Screen 0', access_route: 'BlankScreen014599'},
+{ name: 'BlankScreen114600', human_name: 'Blank 2 Screen', access_route: 'BlankScreen114600'},
+{ name: 'BlankScreen014599', human_name: 'Blank 1 Screen', access_route: 'BlankScreen014599'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
